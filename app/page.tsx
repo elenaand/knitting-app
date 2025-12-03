ffd43c469ad6f21dccc3d0055cb9a56e588fc2e1
Elena Andrés <elena.andres@tv2.no>
2025-12-03 19:50:40 +0100

    fix: change name -  Ulliverset is a existing concept

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full flex-col items-center gap-12 px-4 py-8 md:py-32 md:px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-wine dark:text-zinc-50">
-            Ulliverset
+            Elena&apos;s Ullivers
           </h1>
           <p className="max-w-md text-lg leading-8 dark:text-zinc-400">
             Få (f)ull overstrikkt 🧶
